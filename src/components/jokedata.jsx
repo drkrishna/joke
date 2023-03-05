96b3180b4209f15ef308fcdf3562e21f58bae1f4
Krishna <krishnak.thakur2@gmail.com>
2023-03-05 12:14:14 +0530

    added the joke data array and render the jokes

---
+++ src/components/jokedata.jsx
@@ -0,0 +1,30 @@
+let JokeData = [
+    {
+        id: 1,
+        question: "this is the question 1",
+        punchline: "This is the punch line 1",
+    },
+    {
+        id: 2,
+        question: "this is the question 2",
+        punchline: "This is the punch line 2",
+    },
+
+    {
+        id: 3,
+        question: "this is the question 3",
+        punchline: "This is the punch line 3",
+    },
+    {
+        id: 4,
+        question: "this is the question 4",
+        punchline: "This is the punch line 4",
+    },
+    {
+        id: 5,
+        question: "this is the question 5",
+        punchline: "This is the punch line 5",
+    },
+];
+
+export default JokeData;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import react from "react";
 import { createRoot } from "react-dom/client";
-import Ajoke from "./components/jokes";
+import JokeData from "./components/jokedata";
+import AJoke from "./components/jokes";
+
 const root = createRoot(document.getElementById("root"));
-root.render(
-  <div>
-    <Ajoke punchLine="this is punch line" />
-    <Ajoke question="this is question 2" punchLine="this is punch line" />
-    <Ajoke question="this is question 3" />
-    <Ajoke question="this is question 4" punchLine="this is punch line" />
-    <Ajoke question="this is question 5" punchLine="this is punch line" />
-  </div>
-);
+
+const jokeComponent = JokeData.map((joke) => {
+  return (
+    <AJoke key={joke.id} question={joke.question} punchLine={joke.punchline} />
+  );
+});
+
+root.render(<div>{jokeComponent}</div>);
