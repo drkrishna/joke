6e1d0addb6b5a40bfbd74547069166e422909a89
Krishna <krishnak.thakur2@gmail.com>
2023-03-05 12:24:59 +0530

    appeneded x after 1p

--- src/components/jokes.jsx
+++ src/components/jokes.jsx
@@ -1,7 +1,7 @@
 import react from "react"
 function Ajoke(props) {
     return (
-        <div style={{ border: "solid 2px red", margin: "1px 1p" }}>
+        <div style={{ border: "solid 2px red", margin: "1px 1px" }}>
             <p style={{ display: !props.question && "none" }}>Question: {props.question}</p>
             <p style={{ display: !props.punchLine && "none" }} > Answer: {props.punchLine}</p>
         </div >
